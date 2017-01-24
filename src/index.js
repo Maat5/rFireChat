7d79a2f9e2db5df96523018ec8383ae796a5b748
Manuel Abrante <abrantemanuel@gmail.com>
2017-01-23 20:01:51 -0400

    init project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/app';
+import config from '../config/config'
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);

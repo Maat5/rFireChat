9b02cc9f40941badff735b04da810c1c6d7ec5c4
Manuel Abrante <abrantemanuel@gmail.com>
2017-03-12 20:04:23 -0400

    join all components

--- src/app/app.chat.js
+++ src/app/app.chat.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import ChatUsersList from './chat.userList';
+import ChatHeader from './header';
+import ChatInput from './chat.input';
+import ChatMessages from './chat.messages';
 
 class AppChat extends Component {
-  render(){
-    return(
-     <div id="app">
-        <h1> logged </h1>
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.user,
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    // get all users
+    firebase.database()
+    .ref('users')
+    .on('child_added', reference => {
+      if(reference.val().email !== this.state.user.email)
+        this.setState({
+          users: this.state.users.concat(reference.val())
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div id="app" >
+         <ChatHeader
+            displayName={ this.state.user.displayName }
+            photoURL = { this.state.user.photoURL }
+            email = { this.state.user.email }
+         />
+         <div className="container-fluid app-content">
+           <ChatUsersList userList={ this.state.users } />
+           <div className="chat-container">
+             <ChatMessages user= { this.state.user } />
+              <ChatInput user= { this.state.user } />
+           </div>
+         </div>
       </div>
     );
   }
--- src/app/app.js
+++ src/app/app.js
@@ -29,7 +29,7 @@ class App extends Component {
       );
     }
     return(
-      <AppChat />
+      <AppChat user={this.state.user} />
     );
   }
 }
